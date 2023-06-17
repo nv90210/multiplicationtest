cdc7573943c40f7be97c72c1098b419b86d33b08
Linus Horn <linushorn@gmx.net>
2023-06-17 04:55:42 +0200

    complete rewrite that generates only as much questions as needed.
    Time to generate 100,000 questions is 125ms

--- simple multiplication.py
+++ simple multiplication.py
@@ -1,37 +1,27 @@
 import random
-import itertools
+import math
+import time
 
 num_of_ques = int(input("Number of questions to ask ? "))
+t1 = time.time()
 score = 0
-#generate a list of numbers from 1 to 10
-nums = list(range(1,10))
+highest_num = math.ceil(math.sqrt(num_of_ques)) # highest number to be multiplied, such that we have enough questions
+nums = list(range(1, max(10, highest_num+1)))
 
-#generate all possible combinations of numbers
-bigset = list(itertools.product(nums,nums))
+questions = []
+for i in range(num_of_ques):
+	questions.append((random.choice(nums), random.choice(nums)))
 
+print(int((time.time()-t1)*1000), "ms")
 
-  
-while num_of_ques > len(bigset):
-	num_of_ques = int(input("Please reduce the number of questions: "))
- 
-#select random questions from all available options
-ques = random.sample(bigset,num_of_ques)
+for idx, (a, b) in enumerate(questions):
+    result = a * b
+    
+    ans = input(f"{idx+1}) {a} x {b} = ")
+    
+    if ans == str(result):
+        score += 1
+    else:
+        print("Wrong! The correct answer is", result)
 
-print("\nAnswer the following:\n")
-
-# loop through the questions
-for idx, question in enumerate(ques):
-    # calculate the result
-	result = question[0] * question[1]
-
-	q_str = str(idx+1) + ") " + str(question[0]) + " x " + str(question[1]) + " = "
-	ans = input(q_str)
-	# check if the answer is correct
-	if ans == str(result):
-		score += 1
-	else:
-		print("Wrong! The correct answer is", result)
-
-percentage = int((score/num_of_ques) * 100)
-print("\nScore =", score, "out of", num_of_ques, "which is", percentage, "%")
-input()
+print(f"Your score is {score} out of {num_of_ques}, which is {int(score/num_of_ques*100)}%")
