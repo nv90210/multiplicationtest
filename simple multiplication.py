8db3f3d518b5023590067c54440258b5188aafe2
Linus Horn <linushorn@gmx.net>
2023-06-17 04:47:42 +0200

    small rewrite, same functionality

--- simple multiplication.py
+++ simple multiplication.py
@@ -1,25 +1,37 @@
 import random
-num_of_ques = 10
+import itertools
+
 num_of_ques = int(input("Number of questions to ask ? "))
-bigset = []
 score = 0
-first = second = [1,2,3,4,5,6,7,8,9,10]
-for i in first:
-	for j in second:
-		#populate all possible queations in a list for random selection
-		bigset.append([i,j])
+#generate a list of numbers from 1 to 10
+nums = list(range(1,10))
+
+#generate all possible combinations of numbers
+bigset = list(itertools.product(nums,nums))
+
+
+  
 while num_of_ques > len(bigset):
 	num_of_ques = int(input("Please reduce the number of questions: "))
+ 
 #select random questions from all available options
-ques=random.sample(bigset,num_of_ques)
+ques = random.sample(bigset,num_of_ques)
 
-len = len(ques)
 print("\nAnswer the following:\n")
-for qloop in range (0,len):
-	firstnum = ques[qloop][0]
-	secondnum = ques[qloop][1]
-	ans = input(str(qloop+1) + ") " + str(firstnum) + " x " + str(secondnum) + " = ")	
-	if ans == str(firstnum * secondnum):
-		score=score +1
-print("\nScore = " + str(score) + "/" + str(num_of_ques))
+
+# loop through the questions
+for idx, question in enumerate(ques):
+    # calculate the result
+	result = question[0] * question[1]
+
+	q_str = str(idx+1) + ") " + str(question[0]) + " x " + str(question[1]) + " = "
+	ans = input(q_str)
+	# check if the answer is correct
+	if ans == str(result):
+		score += 1
+	else:
+		print("Wrong! The correct answer is", result)
+
+percentage = int((score/num_of_ques) * 100)
+print("\nScore =", score, "out of", num_of_ques, "which is", percentage, "%")
 input()
